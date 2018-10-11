77af6b17c14895b072cf1c3b8e8c7a35ef69c289
itthaovn <itthaovn.990@gmail.com>
2018-10-11 08:55:47 +0700

    them nut -

--- Git_GitHub/Form1.Designer.cs
+++ Git_GitHub/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.linkLabel3 = new System.Windows.Forms.LinkLabel();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -74,12 +75,12 @@
             // linkLabel3
             // 
             this.linkLabel3.AutoSize = true;
-            this.linkLabel3.Location = new System.Drawing.Point(88, 128);
+            this.linkLabel3.Location = new System.Drawing.Point(88, 179);
             this.linkLabel3.Name = "linkLabel3";
-            this.linkLabel3.Size = new System.Drawing.Size(88, 17);
+            this.linkLabel3.Size = new System.Drawing.Size(60, 17);
             this.linkLabel3.TabIndex = 5;
             this.linkLabel3.TabStop = true;
-            this.linkLabel3.Text = "Result 1 +2 :";
+            this.linkLabel3.Text = "Result  :";
             // 
             // textBox3
             // 
@@ -90,18 +91,29 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(184, 179);
+            this.button1.Location = new System.Drawing.Point(173, 179);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 6;
-            this.button1.Text = "Tinh";
+            this.button1.Text = "+";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(254, 179);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(551, 286);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.linkLabel3);
             this.Controls.Add(this.textBox3);
@@ -125,6 +137,7 @@
         private System.Windows.Forms.LinkLabel linkLabel3;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
